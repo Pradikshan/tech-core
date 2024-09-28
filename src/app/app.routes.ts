16331ee1a1603bd5de75541f7edab8b2298525ed
Pradikshan <pradikshan234@gmail.com>
2024-09-28 22:02:14 +0530

    Added product route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent, title: 'Home - Tech Core' },
+  {
+    path: 'product',
+    component: ProductComponent,
+    title: 'Product - Tech Core',
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
