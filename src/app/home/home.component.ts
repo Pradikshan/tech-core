eb8647c8213f0ab22577c782ee96b39ab955886f
Pradikshan <pradikshan234@gmail.com>
2024-09-29 19:44:39 +0530

    MAde the layout responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamComponent } from '../team/team.component';
 import { FeatureComponent } from '../feature/feature.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, TeamComponent, FeatureComponent],
+  imports: [CommonModule, RouterLink, TeamComponent, FeatureComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
